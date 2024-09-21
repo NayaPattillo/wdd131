f2e79e3c1cfd6a525bbb5ee40042ca8302ac0721
NayaPattillo <pat22019@byui.edu>
2024-09-21 13:06:21 -0600

    javascript

---
+++ scripts/getdates.js
@@ -0,0 +1,7 @@
+const year = document.querySelector("#currentyear")
+const today = new Date();
+year.innerHTML = today.getFullYear();
+
+const lastMod = document.querySelector("#lastModified")
+const modDate = new Date(document.lastModified);
+lastMod.innerHTML = ` - Last Modified: ${modDate}`;
