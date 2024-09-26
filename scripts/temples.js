c13bfa4151bf85077793efb1bf7181d8c89b259c
NayaPattillo <pat22019@byui.edu>
2024-09-26 13:07:58 -0600

    temples assignment

---
+++ scripts/temples.js
@@ -0,0 +1,15 @@
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+    navigation.classList.toggle('open');
+    hamButton.classList.toggle('open');
+});
+
+const year = document.querySelector("#currentyear")
+const today = new Date();
+year.innerHTML = today.getFullYear();
+
+const lastMod = document.querySelector("#lastModified")
+const modDate = new Date(document.lastModified);
+lastMod.innerHTML = ` - Last Modified: ${modDate}`;
